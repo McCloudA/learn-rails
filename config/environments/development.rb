a644517bf80f4911aa4eea460f74e5b0d3b8f3a2
Aaron McCloud <mcclouda@gmail.com>
2014-10-29 19:43:16 -0700

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,33 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = true 
+
+
+
+
+
+  #EVERYTHING FOLLOWING IS PART OF THE RAIL APP TUTORIAL
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+  }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
+
+
+
+
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
